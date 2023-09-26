a93f0ca049ae3899cd18cdc42331c834e49d7695
homoakin619 <homohaykins619@gmail.com>
2023-09-26 20:32:29 +0100

    deployment commit

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+app.use
 
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","html")
+nunjucks.configure("views",{
+    autoescape:true,express:app
+})
+
+app.use("/",(req,res) => {
+  res.render("index")
+})
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
